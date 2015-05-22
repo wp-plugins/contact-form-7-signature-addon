299faa8d4f6527294c834cb909b92ef35167f5eb
tameroski <tameroski@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-22 07:25:49 +0000

    Bug fix : form submission was blocked in some cases
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7-signature-addon/tags/2.2@1165518 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- signature.php
+++ signature.php
@@ -5,7 +5,7 @@ Plugin URI:
 Description: Add signature field type to the popular Contact Form 7 plugin.
 Author: Breizhtorm
 Author URI: http://www.breizhtorm.fr
-Version: 2.1
+Version: 2.2
 */
 
 // this plugin needs to be initialized AFTER the Contact Form 7 plugin.
